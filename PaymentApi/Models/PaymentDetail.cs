b930fd8cd68b375ff340611bc439682d4d694018
SaadZaheer <SaadZaheer@MS-SAAD-ZAHEER->
2022-07-26 13:05:39 +0500

    Add project files.

---
+++ PaymentApi/Models/PaymentDetail.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentApi.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailsId { get; set; }
+        public string CardOwnerName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }
+    }
+}
